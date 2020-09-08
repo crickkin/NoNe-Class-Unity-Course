c583f89469c31f917688094a353d891bedc43384
crickkin <37256927+crickkin@users.noreply.github.com>
2020-09-08 16:24:44 -0300

    aula 122

---
+++ Assets/Flappy Plane/Scripts/ObstacleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    [Range(0.01f, 50f)] public float speed = 2f;
+    
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
--- Assets/Flappy Plane/Scripts/PlayerController.cs
+++ Assets/Flappy Plane/Scripts/PlayerController.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(0.01f, 50f)] public float soarForce = 2f;
+    [Range(0.0f, 4f)] public float fallSpeedOffset = 0.5f;
+    private Rigidbody2D rb2d;
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Boost();
+        LimitFall();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Boost()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rb2d.velocity = Vector2.up * soarForce;
+        }
+    }
+
+    private void LimitFall()
+    {
+        if (rb2d.velocity.y < -(soarForce + fallSpeedOffset))
+        {
+            rb2d.velocity = Vector2.down * soarForce;
+        }
     }
 }
